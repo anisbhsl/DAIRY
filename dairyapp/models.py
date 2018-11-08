3352211698aa998d492069b232368ed8b9c257b7
bhusal <bhusal.anish12@gmail.com>
2018-11-08 13:41:39 +0545

    switched to backend

--- dairyapp/models.py
+++ dairyapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Customer(models.Model):
+    customer_id=models.AutoField(primary_key=True)
+    customer_name=models.TextField(max_length=50)
+    customer_address=models.TextField(max_length=50)
+    customer_contact=models.IntegerField(max_length=10)
+
+
+    def __str__(self):
+        return self.customer_name
+
+
+class Product(models.Model):
